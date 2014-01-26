d77330d1ab6c216d188cfc1c98cd1644333f3645
TomGroombridge <groombridgetom@gmail.com>
2014-01-26 20:15:32 +0000

    user can sign in and leave comment under own name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
-class CommentsController < ApplicationController
+	class CommentsController < ApplicationController
 
 	def new
 		@post = Post.find params[:post_id]
-		@comment = Comment.new
+		@comment = Comment.new(post: @post)
 	end
 
 	def create
 		@post = Post.find params[:post_id]
 		@post.comments << Comment.create(params[:comment].permit(:title, :content))
+		# @comment.post = @post
+		# @comment.user = current_user
 		redirect_to post_path(@post)
 	end
 end
